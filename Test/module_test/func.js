b0c1306b6e4462a3e7f0817ebd7cbc3aaf265f9d
Kitae's Github <aqua0405@gmail.com>
2022-11-16 01:26:33 +0900

    Initial commit

---
+++ Test/module_test/func.js
@@ -0,0 +1,14 @@
+const {odd, even} = require('./var');
+//import {odd, even} from './var';
+console.log(odd, even);
+
+function checkOddOrEven(number) {
+    if(number % 2){
+        return odd;
+    } else {
+        return even;
+    }
+}
+
+module.exports = checkOddOrEven;
+//export default checkOddOrEven;
